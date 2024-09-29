4c14628ee89a42399c327a77bb01a7b91ee5ff0b
TEJAS N <151165402+tejtejasn@users.noreply.github.com>
2024-09-29 12:35:55 +0000

    conversion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score)); //FUnction
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true =? 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+/* 1 => true, 0=> false
+"" => false
+"Tej" => true
+*/
+
+let someNumber= 33
+let stringNumber=String(someNumber)
+console.log(stringNumber;
+console.log(typeof stringNumber);
