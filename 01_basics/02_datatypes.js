cff63a0951a89859d09e199c21eb374425d3420a
TEJAS N <151165402+tejtejasn@users.noreply.github.com>
2024-09-29 12:20:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treats all JS Code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// Readbility is Important
+console.log(3 + 3)
+console.log(3 - 3)
+console.log(3 * 3)
+console.log(3 / 3)
+console.log(3 % 3)
+console.log("Tej")
+
+let name = "Tej" //string
+let age= 21      // Int
+let isLoggedIn= false // Boolean
+
+// number
+// bigint
+// string
+// boolean
+// null = Standalone value
+// undefined
+// symbol => unique
+
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined/unassigned
+console.log(typeof Object);
